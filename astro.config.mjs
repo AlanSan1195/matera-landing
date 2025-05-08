4bbeda47a7af0554ea34d8b995af85db85f25954
Alan San <alansandovalt1@gmail.com>
2025-05-08 16:03:35 -0600

    add: componentes SingIn SignedOut of clerk

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,17 @@ import tailwind from "@astrojs/tailwind";
 import node from "@astrojs/node";
 // autentificacion con clerk
 import clerk from "@clerk/astro";
+import { neobrutalism } from "@clerk/themes";
+import { esES } from "@clerk/localizations";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), clerk()],
+  integrations: [tailwind(), clerk({
+    localizations: esES,
+    appearance: {
+      baseTheme: neobrutalism
+    }
+  })],
   adapter: node({ mode: "standalone" }),
   output: "server",
 });
